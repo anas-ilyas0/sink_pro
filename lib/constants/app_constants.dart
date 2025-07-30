94b4f87a543afee1859664a79db99bc50bbb2257
MUHAMMAD ANAS FAIZAN <manasfaizan0786@gmail.com>
2025-07-30 14:12:06 +0500

    sink_pro webview

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const screenW = 390.0;
+  static const screenH = 844.0;
+  static const String appName = 'Sink Pro';
+  static const String sinkProUrl =
+      'https://staging.waterstreet.net/simplysinks/mobile';
+  static const String privacyPolicy =
+      'https://simplysinks.waterstreet.net/privacy_statement.html';
+  static const String poppins = 'Poppins';
+}
---
+++ lib/constants/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const _base = 'assets/images/';
+  static const String sinkPro = '${_base}sinkpro.png';
+  static const String sinkProLink = '${_base}sinkprolink.png';
+  static const String checkBox = '${_base}checkBox.png';
+}
